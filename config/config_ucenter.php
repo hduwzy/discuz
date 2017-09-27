54d3100ed37b76f7e9cd766ab62404abef94b9ac
wenziyue <wenziyue@mifengkong.cn>
2017-09-27 14:53:26 +0800

    init

---
+++ config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ultrax`.ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', '3cA6R8Q4T52dC9h8B6Y5O3vbEaUa390eocG1y5dblfAdLcE864f8u0w8c1Cft2c5');
+define('UC_API', 'http://discuz.local:8080/uc_server');
+define('UC_APPID', '');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
